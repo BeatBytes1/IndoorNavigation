85afe55281d2a76fecf450c6e5786808a4ec615e
hyattn <hayatabasketball112098@gmail.com>
2023-09-05 14:46:39 +0900

    Current Location Find with using both Vuforia and AR foundation camera (2camera)

---
+++ CustomDefaultTrackanleEventHandler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class CustomDefaultTrackableEventHandler : DefaultObserverEventHandler
+{
+
+    [SerializeField]
+    public GameObject AppearObject;
+    /*
+    [SerializeField]
+    public GameObject Kitchen;
+    */
+
+    [SerializeField]
+    public Transform TargetObject;
+ 
+    [SerializeField]
+    public Transform arCamera;
+    [SerializeField]
+    public Transform indicator;
+
+
+    public UnityEvent OnTrackingAction;
+    public UnityEvent OffTrackingAction;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        OnTrackingAction.Invoke();
+        //if (AppearObject.activeSelf)
+        //{
+            PlaceObjectsAtImagePosition(TargetObject.transform.position);
+        //}
+    }
+
+
+
+    private void PlaceObjectsAtImagePosition(Vector3 imagePosition)
+    {
+        arCamera.position = imagePosition;
+        indicator.position = imagePosition;
+
+        if (imagePosition != TargetObject.position)
+        {
+            TargetObject.position = imagePosition;
+        }
+       
+    }
+
+}
